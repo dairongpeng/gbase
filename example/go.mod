7298aa9b93bd5c17408f88e640f131f6f4d90365
dai <darope@163.com>
2021-09-14 11:18:28 +0800

    fix example

--- example/go.mod
+++ example/go.mod
@@ -3,3 +3,7 @@ module github.com/gopher-core/base/example
 go 1.16
 
 require github.com/gopher-core/base v1.0.1
+
+replace (
+	github.com/gopher-core/base v1.0.1 => ../
+)
--- example/main.go
+++ example/main.go
@@ -1,11 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"github.com/gopher-core/base"
-)
+import "fmt"
 
 func main() {
-	cfg := base.Config()
-	fmt.Println(cfg.Get("mysql.web.addr"))
+	t := GetTicket("你好", "世界")
+	fmt.Println(t.A)
+	fmt.Println(t.B)
+}
+
+type Ticket struct {
+	A string
+	B string
+}
+
+func GetTicket(hello ,world string) *Ticket {
+	t := Ticket{
+		A: hello,
+		B: world,
+	}
+	return &t
 }
