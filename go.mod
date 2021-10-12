9e357e2d791dd27e6766c7d71b2f21125d82584b
dairongpeng <rongpengdai@163.com>
2021-10-12 16:30:33 +0800

    add kafka produce

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/dairongpeng/gbase
 go 1.16
 
 require (
+	github.com/Shopify/sarama v1.30.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/rs/zerolog v1.25.0
 	github.com/spf13/viper v1.8.1
 	github.com/uptrace/bun v1.0.11
 	github.com/uptrace/bun/dialect/mysqldialect v1.0.11
+	go.opentelemetry.io/contrib/instrumentation/github.com/Shopify/sarama/otelsarama v0.25.0
 )
--- kafka.go
+++ kafka.go
@@ -1,1 +1,47 @@
 package gbase
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+// NewKafkaConfig 获取sarama默认配置
+func NewKafkaConfig() *sarama.Config {
+	// sarama默认给的配置
+	c := sarama.NewConfig()
+	// 默认消息不丢
+	c.Producer.RequiredAcks = sarama.WaitForAll
+	// 消费者默认从last消费位消费
+	c.Consumer.Offsets.Initial = sarama.OffsetOldest
+	return c
+}
+
+// NewKafkaClient 获取Kafka的Client
+func NewKafkaClient(name string, config *sarama.Config) (sarama.Client, error) {
+	addrs := Cfg().GetStringSlice(name + ".addrs")
+	client, err := sarama.NewClient(addrs, config)
+	if err != nil {
+		return nil, fmt.Errorf("new sarama client: %w", err)
+	}
+	return client, nil
+}
+
+// NewSyncProducer 创建同步Producer
+func NewSyncProducer(client sarama.Client) (sarama.SyncProducer, error) {
+	producer, err := sarama.NewSyncProducerFromClient(client)
+	if err != nil {
+		return nil, fmt.Errorf("new sync producer from client: %w", err)
+	}
+	// return otelsarama.WrapSyncProducer(client.Config(), producer), nil
+	return producer, nil
+}
+
+// NewAsyncProducer 返回异步Producer
+func NewAsyncProducer(client sarama.Client) (sarama.AsyncProducer, error) {
+	producer, err := sarama.NewAsyncProducerFromClient(client)
+	if err != nil {
+		return nil, fmt.Errorf("new async producer from client: %w", err)
+	}
+	// return otelsarama.WrapAsyncProducer(client.Config(), producer), nil
+	return producer, nil
+}
