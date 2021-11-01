6af01c9bf825dbd1cf7ab13efc9ed971ce8dfe5b
dairongpeng <rongpengdai@163.com>
2021-11-01 11:12:02 +0800

    cron

--- example/main.go
+++ example/main.go
@@ -21,9 +21,6 @@ func main() {
 	cron.Start()
 
 	quit := make(chan os.Signal, 1)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 }
