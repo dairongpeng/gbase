07c572ee8571eac14ed4581d740ed7866a1172da
dairongpeng <rongpengdai@163.com>
2021-11-25 23:27:20 +0800

    fff

--- example/main.go
+++ example/main.go
@@ -3,24 +3,9 @@ package main
 import (
 	"fmt"
 	_ "github.com/dairongpeng/gbase"
-	"github.com/robfig/cron"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
-	cron := cron.New()
-	//nolint
-	err := cron.AddFunc("0 * * * * ?", func() {
-		fmt.Println("per s")
-	})
-	if err != nil {
-		panic(err)
-	}
-	cron.Start()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
+	a := "5f45f6f3cbc9590001063ba4"
+	fmt.Println(len(a))
 }
